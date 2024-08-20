6d4a4285d5f95c8f0ed75b2173b699da876d74ad
Karl Heinz Struggl <karl.struggl@sentry.io>
2024-08-20 17:24:15 +0200

    cleanup

--- lib/src/settings/settings_view.dart
+++ lib/src/settings/settings_view.dart
@@ -9,10 +9,6 @@ class SettingsView extends StatelessWidget {
 
   static const routeName = '/settings';
 
-  // final SettingsController controller;
-
-  // TODO add logout, settings, etc.
-
   @override
   Widget build(BuildContext context) {
     return Consumer(
@@ -30,30 +26,6 @@ class SettingsView extends StatelessWidget {
                 context.go('/signIn');
               },
             ),
-            // Glue the SettingsController to the theme selection DropdownButton.
-            //
-            // When a user selects a theme from the dropdown list, the
-            // SettingsController is updated, which rebuilds the MaterialApp.
-            // child: DropdownButton<ThemeMode>(
-            //   // Read the selected themeMode from the controller
-            //   value: controller.themeMode,
-            //   // Call the updateThemeMode method any time the user selects a theme.
-            //   onChanged: controller.updateThemeMode,
-            //   items: const [
-            //     DropdownMenuItem(
-            //       value: ThemeMode.system,
-            //       child: Text('System Theme'),
-            //     ),
-            //     DropdownMenuItem(
-            //       value: ThemeMode.light,
-            //       child: Text('Light Theme'),
-            //     ),
-            //     DropdownMenuItem(
-            //       value: ThemeMode.dark,
-            //       child: Text('Dark Theme'),
-            //     )
-            //   ],
-            // ),
           ),
         );
       },
