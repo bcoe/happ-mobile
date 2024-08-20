f30476136dce812848ddce1d69c7809de478fe4b
Karl Heinz Struggl <karl.struggl@sentry.io>
2024-08-20 17:08:07 +0200

    PoC logout

--- lib/src/sign_in/login_client.g.dart
+++ lib/src/sign_in/login_client.g.dart
@@ -39,5 +39,19 @@ final authenticateGoogleProvider =
 
 typedef AuthenticateGoogleRef
     = AutoDisposeFutureProviderRef<Map<String, String>?>;
+String _$logoutHash() => r'1254e8a9bfb543d625018971b123b90881bed899';
+
+/// See also [logout].
+@ProviderFor(logout)
+final logoutProvider = AutoDisposeFutureProvider<Map<String, String>?>.internal(
+  logout,
+  name: r'logoutProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$logoutHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef LogoutRef = AutoDisposeFutureProviderRef<Map<String, String>?>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
