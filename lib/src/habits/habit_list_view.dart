8423fd395f6c9068bab391f238831aab80f54111
Karl Heinz Struggl <karl.struggl@sentry.io>
2024-08-21 13:18:34 +0200

    habitlist: swipe to delete, date in header

--- lib/src/habits/habit_list_view.dart
+++ lib/src/habits/habit_list_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:happ_flutter/src/habits/habit_provider.dart';
+import 'package:intl/intl.dart';
 
 import 'habit.dart';
 
@@ -10,22 +11,29 @@ class HabitListView extends StatelessWidget {
 
   static const routeName = '/habits';
 
+  Future<void> deleteAndRefreshHabits(WidgetRef ref, Habit habit) async {
+    await ref.read(deleteHabitProvider(habit.habitId).future);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer(builder: (context, ref, child) {
       return Scaffold(
-        appBar: AppBar(title: const Text('Habits'), actions: [
-          IconButton(
-              icon: const Icon(Icons.add),
-              onPressed: () {
-                context.go('/habits/add');
-              }),
-          IconButton(
-              icon: const Icon(Icons.settings),
-              onPressed: () {
-                context.go('/habits/settings');
-              }),
-        ]),
+        appBar: AppBar(
+            title:
+                Text('Habits for ${DateFormat.yMMMd().format(DateTime.now())}'),
+            actions: [
+              IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () {
+                    context.go('/habits/add');
+                  }),
+              IconButton(
+                  icon: const Icon(Icons.settings),
+                  onPressed: () {
+                    context.go('/habits/settings');
+                  }),
+            ]),
         body: RefreshIndicator(
           onRefresh: () async => ref.refresh(habitsProvider.future),
           child: FutureBuilder<List<Habit>>(
@@ -34,7 +42,7 @@ class HabitListView extends StatelessWidget {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return const Center(child: CircularProgressIndicator());
               } else if (snapshot.hasData) {
-                return buildHabits(snapshot.data!);
+                return buildHabits(snapshot.data!, ref);
               } else {
                 return const Center(child: Text("No data available"));
               }
@@ -45,25 +53,33 @@ class HabitListView extends StatelessWidget {
     });
   }
 
-  Widget buildHabits(List<Habit> habits) {
+  Widget buildHabits(List<Habit> habits, WidgetRef ref) {
     return ListView.builder(
       restorationId: 'habitsListView',
       itemCount: habits.length,
       itemBuilder: (BuildContext context, int index) {
         final habit = habits[index];
-        return buildHabitItem(habit, context);
+        return buildHabitItem(habit, context, ref);
       },
     );
   }
 
-  ListTile buildHabitItem(Habit habit, BuildContext context) {
-    return ListTile(
-        title: Text(habit.name),
-        leading: const CircleAvatar(
-          foregroundImage: AssetImage('assets/images/flutter_logo.png'), // TODO
-        ),
-        onTap: () {
-          context.go('/habits/edit/${habit.habitId}');
-        });
+  Dismissible buildHabitItem(Habit habit, BuildContext context, WidgetRef ref) {
+    return Dismissible(
+      key: Key(habit.habitId),
+      onDismissed: (direction) {
+        deleteAndRefreshHabits(ref, habit);
+      },
+      background: Container(color: Colors.red),
+      child: ListTile(
+          title: Text(habit.name),
+          leading: const CircleAvatar(
+            foregroundImage:
+                AssetImage('assets/images/flutter_logo.png'), // TODO
+          ),
+          onTap: () {
+            context.go('/habits/edit/${habit.habitId}');
+          }),
+    );
   }
 }
